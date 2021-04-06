5c5869fef1a3f01679f26788456c68870087df65
Sakura Phansiri <you@example.com>
2021-04-05 19:41:48 -0500

    sql

--- queries.sql
+++ queries.sql
@@ -9,11 +9,6 @@ SELECT name FROM employer WHERE location = "St. Louis City";
 DROP TABLE job_skills;
 
 ## Part 4: Test it with SQL
-SELECT name, description
-FROM skill
-LEFT JOIN job_skills ON  skill.id = job_skills.skills_id
-WHERE job_skills.jobs_id = "21"
-ORDER BY name;
 
 SELECT jobs_id, name, description
 FROM skill
